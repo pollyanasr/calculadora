fa7f9e31e016ea6ac6a5757372992275b0066513
Pollyana Rodrigues <pollyanna.fla@gmail.com>
2023-09-04 15:59:34 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,167 @@
+
+
+import Button from './components/Button';
+import Input from './components/Input';
+
+import {Container, Content, Row} from './style.js';
+import { useState } from 'react';
+
+const App = () => {
+
+  const [currentNumber, setCurrentNumber] = useState  ('0');
+  
+  const [firstNumber, setFirstNumber] = useState('0');
+
+  const [operation, setOperation] = useState('');
+  //ADICIONAR NUMEROS AO INPUT
+  const handleAddNumber = (num) => {
+    setCurrentNumber(prev => `${prev === '0'? '' : prev}${num}`)
+  }
+
+
+  //BOTÃO 'C' - LIMPAR A CALCULADORA
+  const handleClear = () => {
+    setCurrentNumber('0');
+    setFirstNumber('0');
+    setOperation('')
+    
+  };
+
+  // FUNÇÃO SOMAR   
+  const handleSumNumbers = () => {
+       
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('+');
+    }else {
+      const sum = Number(firstNumber) + Number(currentNumber)
+      setCurrentNumber(String(sum));
+      setOperation('');
+      
+    } 
+
+  }
+
+    // FUNÇÃO SUBTRAIR   
+    const handleSubNumbers = () => {
+       
+      if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0');
+        setOperation('-');
+      }else {
+        const sum = Number(firstNumber) - Number(currentNumber)
+        setCurrentNumber(String(sum));
+        setOperation('');
+        
+      } 
+  
+    }
+
+    // FUNÇÃO MULTIPLICAR   
+    const handleMulNumbers = () => {
+       
+      if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0');
+        setOperation('*');
+      }else {
+        const sum = Number(firstNumber) * Number(currentNumber)
+        setCurrentNumber(String(sum));
+        setOperation('');
+        
+      } 
+  
+    }
+
+    // FUNÇÃO DIVIDIR   
+    const handleDivNumbers = () => {
+       
+      if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0');
+        setOperation('/');
+      }else {
+        const sum = Number(firstNumber) / Number(currentNumber)
+        setCurrentNumber(String(sum));
+        setOperation('');
+        
+      } 
+  
+    }
+
+  const handleEquals= () => {
+       
+    if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
+      switch(operation){
+        case '+':
+          handleSumNumbers();
+          break;
+        case '-':
+          handleSubNumbers();
+          break;
+        case '*':
+          handleMulNumbers();
+          break;
+        case '/':
+          handleDivNumbers();
+          break;
+        default:
+          break;
+      }
+   
+      
+    };
+
+
+  }
+
+
+  
+  
+  return (
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+
+        <Row>
+          <Button label={1}    onClick={() => handleAddNumber('1')}/>
+          <Button label={2}    onClick={() => handleAddNumber('2')}/>
+          <Button label={3}    onClick={() => handleAddNumber('3')}/>
+          <Button label={"x"}  onClick={() => handleMulNumbers()}/>
+        </Row>
+
+        <Row>
+          <Button label={4}     onClick={() => handleAddNumber('4')}/>
+          <Button label={5}     onClick={() => handleAddNumber('5')}/>
+          <Button label={6}     onClick={() => handleAddNumber('6')}/>
+          <Button label={"+"}   onClick={() => handleSumNumbers()}/>
+        </Row>
+
+        <Row>
+          <Button label={7}     onClick={() => handleAddNumber('7')}/>
+          <Button label={8}     onClick={() => handleAddNumber('8')}/>
+          <Button label={9}     onClick={() => handleAddNumber('9')}/>
+          <Button label={"-"}   onClick={() => handleSubNumbers()}/>
+        </Row>
+        
+
+        <Row>
+          <Button label={"c"}  onClick={() => handleClear()}/>
+          
+          <Button label={"0"}  onClick={() => handleAddNumber('0')} />
+          <Button label={"="}  onClick={() => handleEquals()}/>
+          
+          <Button label={"/"}  onClick={() => handleDivNumbers()}/>
+          
+          
+        </Row>
+        
+        
+      </Content> 
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const  ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #6acfd2;
+    color: #FFF;
+    font-size: 24px;
+    font-weight: 700; 
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`;
